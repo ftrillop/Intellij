69bcd2d6e02f708ee8212628802cbe294c8beeee
ftperez <ftperez7@gmail.com>
2022-02-08 09:10:33 +0100

    Commit v2 Fernando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Persona p = new Persona();
+        p.nombre = "Pepa";
+        p.edad = 18;
+        p.estatura = 1.89;
+        System.out.println(p.nombre);
     }
 }
